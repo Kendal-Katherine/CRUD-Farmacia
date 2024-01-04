c2ea0d6922a6a374650759ee32a03518ae32355d
Kendal Katherine <kendal.katherine3@gmail.com>
2024-01-04 12:17:59 -0300

    CRUD completo do recurso Produto

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,11 +1,18 @@
 package com.generation.farmacia.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -20,6 +27,9 @@ public class Categoria {
 	@Column(length = 100)
 	@NotNull(message = "O atributo tipo é obrigatório!")
 	private String tipo;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	private List<Produtos> produto = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -36,5 +46,16 @@ public class Categoria {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	public List<Produtos> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produtos> produto) {
+		this.produto = produto;
+	}
+
+	
+	
 
 }
